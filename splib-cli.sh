0dac4bcd7a0aa1609915b2ed0185c2d968fdab16
heecheol.park <heecheol.park@linecorp.com>
2022-11-16 23:48:32 +0900

    Compatibility support for the sed command

--- splib-cli.sh
+++ splib-cli.sh
@@ -6,19 +6,19 @@ curl -c c.txt -d "userId=$1&password=$2" \
 curl -s -b c.txt https://splib.or.kr/intro/index.do > out.txt 
 # user name
 grep "barcodeInfo" out.txt \
-| sed 's/<span.*>//' \
-| sed 's/<.*\">//' \
+| sed 's|<span.*>||' \
+| sed 's|<.*\">||' \
 | awk '{$1=$1;print}'
 # loan count
 grep "/intro/program/mypage/loanStatusList.do" out.txt \
-| sed 's/<\/span>.*>//' \
-| sed 's/<a.*">//' \
-| sed 's/ <span>/:/' \
+| sed 's|<\/span>.*>||' \
+| sed 's|<a.*">||' \
+| sed 's| <span>|:|' \
 | awk '{$1=$1;print}'
 # doorae count
 grep "/intro/program/mypage/dooraeLillStatusList.do" out.txt \
-| sed 's/<\/span>.*>//' \
-| sed 's/<a.*">//' \
-| sed 's/ <span>/:/' \
+| sed 's|<\/span>.*>||' \
+| sed 's|<a.*">||' \
+| sed 's| <span>|:|' \
 | awk '{$1=$1;print}'
 
