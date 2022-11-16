65ed8848932607c89803b75ea1c1a03369617de4
pchuri <pchuri@gmail.com>
2022-11-16 20:10:58 +0900

    Remove unused commands

--- splib-cli.sh
+++ splib-cli.sh
@@ -1,6 +1,4 @@
-curl -c c.txt https://splib.or.kr >/dev/null 2>&1
-curl -b c.txt https://splib.or.kr/intro/program/memberLogout.do >/dev/null 2>&1
-curl -b c.txt -d "userId=$1&password=$2" \
+curl -c c.txt -d "userId=$1&password=$2" \
 -H "Content-Type: application/x-www-form-urlencoded" \
 -X POST "https://splib.or.kr/intro/program/memberLoginProc.do" > /dev/null 2>&1
 
