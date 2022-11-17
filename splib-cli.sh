87830255725b680beac07dfa5a1b490c37ed4dfb
heecheol.park <heecheol.park@linecorp.com>
2022-11-17 22:28:26 +0900

    Use pipeline

--- splib-cli.sh
+++ splib-cli.sh
@@ -2,6 +2,4 @@ curl -c .c.txt -d "userId=$1&password=$2" \
 -H "Content-Type: application/x-www-form-urlencoded" \
 -X POST "https://splib.or.kr/intro/program/memberLoginProc.do" \
 > /dev/null 2>&1
-
-curl -s -b .c.txt https://splib.or.kr/intro/index.do > .out.txt 
-awk -f awk.script .out.txt
+curl -s -b .c.txt https://splib.or.kr/intro/index.do | awk -f awk.script
