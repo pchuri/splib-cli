f843c41f61fc7bc890b43d4c9977856075057291
heecheol.park <heecheol.park@linecorp.com>
2022-11-17 20:13:59 +0900

    Refactoring

--- splib-cli.sh
+++ splib-cli.sh
@@ -4,9 +4,4 @@ curl -c c.txt -d "userId=$1&password=$2" \
 > /dev/null 2>&1
 
 curl -s -b c.txt https://splib.or.kr/intro/index.do > out.txt 
-# user name
-awk -f name.script out.txt
-# loan count
-awk -F '<|>' '/intro\/program\/mypage\/loanStatusList.do/ {sub(/ /,"",$3);print $3":"$5}' out.txt 
-# doorae count
-awk -F '<|>' '/\/intro\/program\/mypage\/dooraeLillStatusList.do/ {sub(/ /,"",$3);print $3":"$5}' out.txt
+awk -f awk.script out.txt
