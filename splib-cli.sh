834cc82e2123df85b859e57312b7e7fcbed8f6c5
heecheol.park <heecheol.park@linecorp.com>
2022-11-17 20:01:13 +0900

    Refactor for extracting name

--- splib-cli.sh
+++ splib-cli.sh
@@ -5,7 +5,7 @@ curl -c c.txt -d "userId=$1&password=$2" \
 
 curl -s -b c.txt https://splib.or.kr/intro/index.do > out.txt 
 # user name
-awk -F '<|>' '/barcodeInfo/ {print $3}' out.txt
+awk -f name.script out.txt
 # loan count
 awk -F '<|>' '/intro\/program\/mypage\/loanStatusList.do/ {sub(/ /,"",$3);print $3":"$5}' out.txt 
 # doorae count
