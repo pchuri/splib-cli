097974a5921759d2657326550e41e2aebd66ae78
pchuri <pchuri@gmail.com>
2022-11-19 00:04:02 +0900

    Use mktemp

--- splib-cli.sh
+++ splib-cli.sh
@@ -1,5 +1,7 @@
-curl -c .c.txt -d "userId=$1&password=$2" \
+tmp=`mktemp -d`
+curl -c "$tmp/c.txt" -d "userId=$1&password=$2" \
 -H "Content-Type: application/x-www-form-urlencoded" \
 -X POST "https://splib.or.kr/intro/program/memberLoginProc.do" \
 > /dev/null 2>&1
-curl -s -b .c.txt https://splib.or.kr/intro/index.do | awk -f awk.script
+curl -s -b "$tmp/c.txt" https://splib.or.kr/intro/index.do | awk -f awk.script
+rm -rf "$tmp"
