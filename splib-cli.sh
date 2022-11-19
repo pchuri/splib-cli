cb38c96722e8ab74dcc20868f6f592fbed113e5a
heecheol.park <heecheol.park@linecorp.com>
2022-11-20 01:04:40 +0900

    [WIP] loan.script

--- splib-cli.sh
+++ splib-cli.sh
@@ -1,5 +1,9 @@
 LOGIN_URL="https://splib.or.kr/intro/program/memberLoginProc.do"
 INDEX_URL="https://splib.or.kr/intro/index.do"
+LOAN_URL="https://splib.or.kr/intro/program/mypage/loanStatusList.do"
 CT="Content-Type: application/x-www-form-urlencoded"
 D="userId=$1&password=$2"
-curl -c - -s -o/dev/null -d "$D" -H "$CT" -X POST $LOGIN_URL | curl -s -b @- $INDEX_URL | awk -f awk.script
+
+curl -c .c.txt -s -o/dev/null -d "$D" -H "$CT" -X POST $LOGIN_URL
+curl -s -b .c.txt $INDEX_URL | awk -f awk.script
+curl -s -b .c.txt $LOAN_URL | awk -f loan.script 
