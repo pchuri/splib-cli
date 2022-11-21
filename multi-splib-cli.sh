26f4cf706370c2f83548a1a4f11acf6dbb047269
heecheol.park <heecheol.park@linecorp.com>
2022-11-21 15:40:59 +0900

    Fix a error

--- multi-splib-cli.sh
+++ multi-splib-cli.sh
@@ -5,7 +5,7 @@ while [ $i -le $length ]
 do
  user=$1
  pw=$2
- source splib-cli.sh $1 $2 >> out.txt
+ sh splib-cli.sh $1 $2 >> out.txt
  echo '========================' >> out.txt
  shift 2
  echo $i
