595121b4b757ad94519de4d515514803dae1847f
heecheol.park <heecheol.park@linecorp.com>
2023-01-05 23:43:20 +0900

    fix:reformat

--- multi-splib-cli.sh
+++ multi-splib-cli.sh
@@ -3,11 +3,11 @@ length=$(($#/2))
 rm -f out.txt
 while [ $i -le $length ]
 do
- user=$1
- pw=$2
- sh splib-cli.sh $1 $2 >> out.txt
- echo '========================' >> out.txt
- shift 2
- printf "Progress : (%d / %d)\r" $i $length
- i=$((i+1))
+    user=$1
+    pw=$2
+    sh splib-cli.sh $1 $2 >> out.txt
+    echo '========================' >> out.txt
+    shift 2
+    printf "Progress : (%d / %d)\r" $i $length
+    i=$((i+1))
 done
