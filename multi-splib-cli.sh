eeb779181621d918c51a895ce92743d4808f6619
pchuri <pchuri@gmail.com>
2023-01-08 22:23:55 +0900

    update:multi-splib-cli

--- multi-splib-cli.sh
+++ multi-splib-cli.sh
@@ -1,13 +1,11 @@
 i=1
 length=$(($#/2))
-rm -f out.txt
 while [ $i -le $length ]
 do
+    echo '========================'
     user=$1
     pw=$2
-    sh splib-cli.sh $1 $2 >> out.txt
-    echo '========================' >> out.txt
+    sh splib-cli.sh $1 $2
     shift 2
-    printf "Progress : (%d / %d)\r" $i $length
     i=$((i+1))
 done
