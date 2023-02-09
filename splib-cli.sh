6f43d2141475cc14afeabc4bbfb7ddd61a51f899
pchuri <pchuri@gmail.com>
2023-02-10 03:44:31 +0900

    Fixes to avoid issues that don't work after an a-shell update

--- splib-cli.sh
+++ splib-cli.sh
@@ -5,7 +5,8 @@ CT="Content-Type: application/x-www-form-urlencoded"
 D="userId=$1&password=$2"
 
 curl -c .c.txt -s -o/dev/null -d "$D" -H "$CT" -X POST $LOGIN_URL
-curl -s -b .c.txt $INDEX_URL | awk -f loan1.awk 
-curl -s -b .c.txt $LOAN_URL | awk -f loan2.awk > .loan.txt
-awk -f sum.awk .loan.txt
+curl -s -b .c.txt $INDEX_URL | awk -f loan1.awk
+ 
+loan=$(curl -s -b .c.txt $LOAN_URL | awk -f loan2.awk)
+echo "$loan" | awk -f sum.awk
 rm -rf .*.txt
