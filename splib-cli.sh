8331279b774bad92de0893f58a688313d5733907
pchuri <pchuri@gmail.com>
2023-02-10 04:16:58 +0900

    Improve performance by storing cookies in variables instead of in files

--- splib-cli.sh
+++ splib-cli.sh
@@ -4,9 +4,8 @@ LOAN_URL="https://splib.or.kr/intro/program/mypage/loanStatusList.do"
 CT="Content-Type: application/x-www-form-urlencoded"
 D="userId=$1&password=$2"
 
-curl -c .c.txt -s -o/dev/null -d "$D" -H "$CT" -X POST $LOGIN_URL
-curl -s -b .c.txt $INDEX_URL | awk -f loan1.awk
+cookies=$(curl -s -c - -d "$D" -H "$CT" -X POST $LOGIN_URL)
+echo "$cookies" | curl -s -b @- $INDEX_URL | awk -f loan1.awk
  
-loan=$(curl -s -b .c.txt $LOAN_URL | awk -f loan2.awk)
+loan=$(echo "$cookies" | curl -s -b @- $LOAN_URL | awk -f loan2.awk)
 echo "$loan" | awk -f sum.awk
-rm -rf .*.txt
